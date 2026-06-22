4be885794cff3b0c94b9281c7f8d3b8ef38cbaf6
praveen <praveenraj9495@gmail.com>
2026-06-22 23:33:37 +0530

    fix: allow prompts.Option on prompt constructors

--- prompt.go
+++ prompt.go
@@ -5,14 +5,14 @@ import (
 	"github.com/hastekit/hastekit-sdk-go/pkg/hastekitgateway"
 )
 
-func (c *SDK) Prompt(prompt string) *prompts.SimplePrompt {
-	return prompts.New(prompt)
+func (c *SDK) Prompt(prompt string, opts ...prompts.PromptOption) *prompts.SimplePrompt {
+	return prompts.New(prompt, opts...)
 }
 
-func (c *SDK) RemotePrompt(name, alias string) *prompts.SimplePrompt {
-	return prompts.NewWithLoader(hastekitgateway.NewExternalPromptPersistence(c.endpoint, c.orgName, c.projectName, name, alias, c.httpClient))
+func (c *SDK) RemotePrompt(name, alias string, opts ...prompts.PromptOption) *prompts.SimplePrompt {
+	return prompts.NewWithLoader(hastekitgateway.NewExternalPromptPersistence(c.endpoint, c.orgName, c.projectName, name, alias, c.httpClient), opts...)
 }
 
-func (c *SDK) CustomPrompt(loader prompts.PromptLoader) *prompts.SimplePrompt {
-	return prompts.NewWithLoader(loader)
+func (c *SDK) CustomPrompt(loader prompts.PromptLoader, opts ...prompts.PromptOption) *prompts.SimplePrompt {
+	return prompts.NewWithLoader(loader, opts...)
 }
